b161023cb2f1a2239c35cdc680107f18dd5e4dbf
Roy Barzilay <roybar03@gmail.com>
2024-01-12 11:41:08 +0200

    Begin implementing camera rig

--- client/src/canvas/CameraRig.tsx
+++ client/src/canvas/CameraRig.tsx
@@ -1,6 +1,8 @@
 import { useRef } from "react";
 import { Group, Object3DEventMap } from "three";
 import { useSnapshot } from "valtio";
+import { easing } from "maath";
+import { useFrame } from "@react-three/fiber";
 
 import state from "../store";
 
@@ -11,6 +13,21 @@ interface CameraRigProps {
 const CameraRig = ({ children }: CameraRigProps) => {
     const groupRef = useRef<Group<Object3DEventMap>>(null);
     const snapshot = useSnapshot(state);
+
+    useFrame((state, delta) => {
+        const isBreakpoint = window.innerWidth <= 1260;
+        const isMobile = window.innerWidth <= 600;
+
+        // set the initial position of the model
+
+        // set the model rotation smoothly
+        easing.dampE(
+            groupRef.current?.rotation!,
+            [state.pointer.y / 10, -state.pointer.x / 5, 0],
+            0.25,
+            delta
+        );
+    });
 
     return <group ref={groupRef}>{children}</group>;
 };
